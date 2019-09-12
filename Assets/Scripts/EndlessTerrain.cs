6b0b74d8b5ea7e1f8fa7f891e57582c20c108e8c
Viraj Sinha <virajosinha@gmail.com>
2019-09-12 08:31:23 -0400

    ⛰️

---
+++ Assets/Scripts/EndlessTerrain.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrain : MonoBehaviour
+{
+    public const float maxViewDistance = 430;
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+    int chunkSize;
+    int chunksVisibleInViewDistance;
+
+    Dictionary<Vector2, TerrainChunk> terrainChunks = new Dictionary<Vector2, TerrainChunk>();
+    List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+
+    private void Start()
+    {
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        chunksVisibleInViewDistance = Mathf.RoundToInt(maxViewDistance / chunkSize);
+    }
+
+    private void Update()
+    {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        UpdateVisibleChunks();
+    }
+
+    void UpdateVisibleChunks()
+    {
+        foreach (var chunk in terrainChunksVisibleLastUpdate) {
+            chunk.SetVisible(false);
+        }
+        terrainChunksVisibleLastUpdate.Clear();
+
+        int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        for (int yOffset = -chunksVisibleInViewDistance; yOffset <= chunksVisibleInViewDistance; yOffset++) {
+            for (int xOffset = -chunksVisibleInViewDistance; xOffset <= chunksVisibleInViewDistance; xOffset++) {
+                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+
+                if (terrainChunks.ContainsKey(viewedChunkCoord)) {
+                    var chunk = this.terrainChunks[viewedChunkCoord];
+                    chunk.UpdateTerrainChunk();
+                    if (chunk.IsVisible()) {
+                        terrainChunksVisibleLastUpdate.Add(chunk);
+                    }
+                } else {
+                    terrainChunks.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize, this.transform));
+                }
+            }
+        }
+    }
+
+    public class TerrainChunk
+    {
+        GameObject meshObject;
+        Vector2 position;
+        Bounds bounds;
+
+        public TerrainChunk(Vector2 coord, int size, Transform parent)
+        {
+            position = coord * size;
+            bounds = new Bounds(position, Vector2.one * size);
+            Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+
+            meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject.transform.position = positionV3;
+            meshObject.transform.localScale = Vector3.one * size / 10f;
+            meshObject.transform.parent = parent;
+            this.SetVisible(false);
+        }
+
+        public void UpdateTerrainChunk()
+        {
+            var viewerDistanceFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+            var visible = viewerDistanceFromNearestEdge <= maxViewDistance;
+            this.SetVisible(visible);
+        }
+
+        public void SetVisible(bool visible)
+        {
+            this.meshObject.SetActive(visible);
+        }
+
+        public bool IsVisible()
+        {
+            return meshObject.activeSelf;
+        }
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode { NoiseMap, ColorMap, Mesh };
+    public DrawMode drawMode;
+
+    public const int mapChunkSize = 241;
+
+    [Range(0, 6)]
+    public int levelOfDetail;
+
+    public float noiseScale = 10;
+
+    [Min(1)]
+    public int octaves = 3;
+
+    [Range(0, 1)]
+    public float persistance = 0.5f;
+
+    public float lacunarity = 5f;
+
+    public int seed;
+    public Vector2 offset;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+
+        for (int y = 0; y < mapChunkSize; y++) {
+            for (int x = 0; x < mapChunkSize; x++) {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++) {
+                    if (currentHeight <= regions[i].height) {
+                        colorMap[y * mapChunkSize + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+
+        if (drawMode == DrawMode.NoiseMap) {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+
+        } else if (drawMode == DrawMode.ColorMap) {
+            var texture = TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize);
+            display.DrawTexture(texture);
+
+        } else if (drawMode == DrawMode.Mesh) {
+            var mesh = MeshGenerator.GenerateTerrainMesh(noiseMap,
+                                                         meshHeightMultiplier,
+                                                         meshHeightCurve,
+                                                         levelOfDetail);
+            var texture = TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize);
+            display.DrawMesh(mesh, texture);
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (lacunarity < 1) { lacunarity = 1; }
+        if (octaves < 0) { octaves = 0; }
+        if (persistance < 0) { octaves = 0; }
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+}
